bd0dc8d2f2b150b8115b71b7fdb2612b0e4ae0a1
julian <julian.batongbacal@gmail.com>
2022-04-09 22:22:10 +1000

    update quotes grammar

--- src/utils/quotes.js
+++ src/utils/quotes.js
@@ -1,31 +1,31 @@
 const quotesJson = [
   {
     from: 'richard buckland, week 1 lecture',
-    quote: 'everyone must read the art of war because it is very clever',
+    quote: 'everyone must read the art of war because it is very clever.',
   },
   {
     from: 'richard buckland',
     quote:
-      'an engineer has a duty to the engineering profession and the public it serves',
+      'an engineer has a duty to the engineering profession and the public they serve.',
   },
   {
     from: 'richard buckland',
     quote:
-      "risk is invisible you're throwing a coin no one ever sees the coin being thrown",
+      "risk is invisible. you're throwing a coin where no one ever sees the coin being thrown.",
   },
   {
     from: 'sun tzu',
-    quote: 'The supreme art of war is to subdue the enemy without fighting',
-  },
-  {
-    from: 'sun tzu',
-    quote:
-      'Let your plans be dark and impenetrable as night, and when you move, fall like a thunderbolt',
+    quote: 'The supreme art of war is to subdue the enemy without fighting.',
   },
   {
     from: 'sun tzu',
     quote:
-      'There is no instance of a nation benefitting from prolonged warfare',
+      'Let your plans be dark and impenetrable as night, and when you move, fall like a thunderbolt.',
+  },
+  {
+    from: 'sun tzu',
+    quote:
+      'There is no instance of a nation benefitting from prolonged warfare.',
   },
   {
     from: 'sun tzu',
