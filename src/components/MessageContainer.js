01d7126f8692027ddafb8682214c51e9741f8a70
julian <julian.batongbacal@gmail.com>
2022-04-10 14:38:51 +1000

    word wrapping done!

--- src/components/MessageContainer.js
+++ src/components/MessageContainer.js
@@ -7,22 +7,65 @@ import LetterBox from '../components/LetterBox';
 const MessageContainer = (props) => {
   const { gameState, handleLetterSelect, selectedLetter } = props;
 
+  // figure out words, put in divs
+  const encryptedValues = gameState.map((letter) => {
+    return letter[0];
+  });
+  const userValues = gameState.map((letter) => {
+    return letter[1];
+  });
+  const encryptedString = encryptedValues.join('');
+  const encryptedWords = encryptedString.split(' ');
+
+  let words = [];
+  // for each word, match userValues over
+  let j = 0;
+  for (let i = 0; i < encryptedWords.length; i++) {
+    let encryptedLetters = encryptedWords[i].split('');
+    let userLetters = userValues.slice(j, j + encryptedWords[i].length);
+    let word = [];
+    for (let x = 0; x < encryptedLetters.length; x++) {
+      word.push([encryptedLetters[x], userLetters[x]]);
+    }
+    j += encryptedWords[i].length;
+    // after each word, move over because of space
+    j += 1;
+    words.push(word);
+  }
+
   return (
     <Container>
-      {gameState.map((letter) =>
-        letter[0] === ' ' ? (
-          <Space />
-        ) : (
-          <LetterBox
-            value={letter[1]}
-            encryptedLetter={letter[0]}
-            handleLetterSelect={handleLetterSelect}
-            selectedLetter={selectedLetter}
-          />
-        )
-      )}
+      {words.map((word) => (
+        <WordContainer>
+          {word.map((letter) => (
+            <LetterBox
+              value={letter[1]}
+              encryptedLetter={letter[0]}
+              handleLetterSelect={handleLetterSelect}
+              selectedLetter={selectedLetter}
+            />
+          ))}
+        </WordContainer>
+      ))}
     </Container>
   );
+
+  // return (
+  //   <Container>
+  //     {gameState.map((letter) =>
+  //       letter[0] === ' ' ? (
+  //         <Space />
+  //       ) : (
+  //         <LetterBox
+  //           value={letter[1]}
+  //           encryptedLetter={letter[0]}
+  //           handleLetterSelect={handleLetterSelect}
+  //           selectedLetter={selectedLetter}
+  //         />
+  //       )
+  //     )}
+  //   </Container>
+  // );
 };
 
 export default MessageContainer;
@@ -38,4 +81,12 @@ const Container = styled.div`
 
 const Space = styled.div`
   width: 45px;
+`;
+
+const WordContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 20px;
+  margin-bottom: 0;
+  margin-top: 0;
 `;
