08b29112d86c74274d0a8e752e98e168c5eb79be
julian <julian.batongbacal@gmail.com>
2022-04-10 17:33:19 +1000

    fix help modal styling

--- src/components/HelpModal.js
+++ src/components/HelpModal.js
@@ -12,28 +12,25 @@ const HelpModal = (props) => {
       <Modal open={open} onClose={handleModalClose}>
         <Container>
           <h2>What is cryptle</h2>
-          <p>
+          <div>
             Cryptle is a resource for learning more about cryptography and
             ciphers, with a cryptogram game for practice.
-          </p>
+          </div>
           <h2>How do I play?</h2>
-          <p>
+          <div>
             A cryptogram is a type of game where you attempt to decrypt an
             encrypted message through substituting letters.{' '}
-          </p>
-          <p>
-            Here are some tips to help you get started on solving:
-            <ul>
-              <li>Common words with single letters: A or I</li>
-              <li>
-                Common words with two letters: IN, IT, IS, AN, AT, AS, SO, OF,
-                OR
-              </li>
-              <li>Common words with three letters: THE, AND, WAS, HAS</li>
-              <li>Two of the same letter together: EE, OO</li>
-              <li>Following apostrophes: S, T, D, LL, VE, RE</li>
-            </ul>
-          </p>
+          </div>
+          <div>Here are some tips to help you get started on solving:</div>
+          <ul>
+            <li>Common words with single letters: A or I</li>
+            <li>
+              Common words with two letters: IN, IT, IS, AN, AT, AS, SO, OF, OR
+            </li>
+            <li>Common words with three letters: THE, AND, WAS, HAS</li>
+            <li>Two of the same letter together: EE, OO</li>
+            <li>Following apostrophes: S, T, D, LL, VE, RE</li>
+          </ul>
         </Container>
       </Modal>
     </div>
